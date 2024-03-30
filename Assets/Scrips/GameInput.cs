b9fe2af210ec2967637f18c63e64dfcfde092e02
NTT203 <anhtienpro203@gmail.com>
2024-03-30 11:04:25 +0700

    Fix moving ,add cinemachine

--- Assets/Scrips/GameInput.cs
+++ Assets/Scrips/GameInput.cs
@@ -2,34 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.PlayerLoop;
 
 public class GameInput : MonoBehaviour
 {
+    
     //singleton pattern
-    public static GameInput instance 
-        { get; set; }
+    // public static GameInput instance 
+    //     { get; set; }
+    // private void Awake(){
+    //     if(instance!=null|| instance!=this){
+    //         Destroy(this);
+    //     }
+    //     else instance=this;
+    // }    
+     PlayerInputSystem playerInputAction;
     private void Awake(){
-        if(instance!=null|| instance!=this){
-            Destroy(this);
-        }
-        else instance=this;
-    }    
+     
+        playerInputAction = new PlayerInputSystem();
+        playerInputAction.Player.Enable();
+    }
     //Get key input and return Vector
     public Vector2 getMovementVector(){
-        Vector2 inputVector= new Vector2(0,0);
-        if(Input.GetKey(KeyCode.W)){
-            inputVector.y+=1;
-        }
-        if(Input.GetKey(KeyCode.S)){
-             inputVector.y-=1;
-        }
-         if(Input.GetKey(KeyCode.A)){
-             inputVector.x-=1;
-        }
-         if(Input.GetKey(KeyCode.D)){
-             inputVector.x+=1;
-        }
+        Vector2 inputVector= playerInputAction.Player.Move.ReadValue<Vector2>();
+       Debug.Log(playerInputAction.Player.Move.ReadValue<Vector2>());
         inputVector=inputVector.normalized;
         return inputVector;
     }
--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -18,6 +18,8 @@ public class Player : MonoBehaviour
         Vector2 inputVector=gameInput.getMovementVector();
         Vector3 moveDir= new Vector3(inputVector.x,0,inputVector.y);
         transform.position+=moveDir*moveSpeed*Time.deltaTime;
+       // Debug.Log(inputVector);
+        //inputVector=inputVector.normalized;
         //rotate Player;
         float rotateSpeed=10f;
         transform.forward=Vector3.Slerp(transform.forward,moveDir,Time.deltaTime*rotateSpeed);
